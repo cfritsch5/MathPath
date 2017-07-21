9872262533b8c1086a563b2d172447ee94765d46
casey fritsch <caseyfritsch@gmail.com>
2017-07-20 17:14:27 -0700

    migrate units table and seed units data

--- db/migrate/20170720234207_create_units.rb
+++ db/migrate/20170720234207_create_units.rb
@@ -1,7 +1,9 @@
 class CreateUnits < ActiveRecord::Migration[5.0]
   def change
     create_table :units do |t|
-
+      t.string :name, null:false
+      t.integer :next_unit
+      t.integer :prev_unit
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(
-  username: "guest",
-  password: "password"
+
+Unit.create!(
+name: "unit 1",
+next_unit: 2,
+prev_unit: nil
+)
+
+Unit.create!(
+name: "unit 2",
+next_unit: 3,
+prev_unit: 1
+)
+
+Unit.create!(
+name: "unit 3",
+next_unit: 4,
+prev_unit: 2
+)
+
+Unit.create!(
+name: "unit 4",
+next_unit: 5,
+prev_unit: 3
+)
+
+Unit.create!(
+name: "unit 5",
+next_unit: 6,
+prev_unit: 4
+)
+
+
+Unit.create!(
+name: "unit 6",
+next_unit: nil,
+prev_unit: 5
 )
