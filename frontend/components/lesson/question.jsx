e3871b7f1a4c415d6018f8f7ee4a0c5a3cf5ac9b
casey fritsch <caseyfritsch@gmail.com>
2017-07-28 12:28:53 -0700

    seed

--- frontend/components/lesson/question.jsx
+++ frontend/components/lesson/question.jsx
@@ -26,7 +26,7 @@ class Question extends React.Component {
     return (
       <div className="question-container">
         <h5 className="questionOutOf">{this.props.idx + 1} of {this.props.numOfquestions}</h5>
-        <h3 className="question-title">{question.name}{" ="}</h3>
+        <h3 className="question-title">{question.name}</h3>
 
         <ul>
           {answersLi}
