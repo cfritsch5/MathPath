b78298128b9657b1a18daef9d159e2a81f6f8fb9
casey fritsch <caseyfritsch@gmail.com>
2017-10-26 21:31:44 -0700

    made units display in levels

--- frontend/components/skilltree/skill_tree.jsx
+++ frontend/components/skilltree/skill_tree.jsx
@@ -15,20 +15,43 @@ class SkillTree extends React.Component {
 
   render() {
     let units = this.props.units || [];
-    const unitItems = Object.keys(units).map((id)=>(
-      <EachUnit
+
+    const unitItems = [];
+    // Object.keys(units).forEach((id)=>(
+    //   unitItems.push(<EachUnit
+    //     key={ id }
+    //     id={id}
+    //     unit={ units[id].name }
+    //     progress={this.props.progress}
+    //     />)
+    //   )) || ["not rendered yet"];
+
+    const ids = Object.keys(units).map((id)=>id);
+    console.log("IDAAAAA", ids);
+    let levels =[];
+    let level = [], id;
+    for(let i = 0 ; i < ids.length ; i++){
+
+      id = ids[i];
+      level.push(<EachUnit
         key={ id }
         id={id}
         unit={ units[id].name }
         progress={this.props.progress}
-        />
-      )) || ["not rendered yet"];
+        />);
 
-
+      if ( i%2 === 0 ){
+        levels.push(level);
+        level = [];
+      }
+    }
+    console.log(levels);
+    let testme = levels.map(_level=><div className="level">{_level}</div>);
     return(<div className="skill-tree">
     <h1>Units: </h1>
       <ul className="unit-index">
         {unitItems}
+        {testme}
       </ul>
     </div>);
   }
